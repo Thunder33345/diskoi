31c4430b42551eeb3b31efbff9a30c52f4c81d18
Thunder33345 <Thunder33345@users.noreply.github.com>
2021-10-27 07:54:16 +0800

    un-flatten cleanup pass

--- types.go
+++ types.go
@@ -16,6 +16,13 @@ type Command interface {
 	applicationCommand() *discordgo.ApplicationCommand
 }
 
+//Mentionable is an instance of something that could be a Role or a User
+//todo improve this type into something like an interface receiver and access functions rather then 2 values
+type Mentionable struct {
+	Role *discordgo.Role
+	User *discordgo.User
+}
+
 type errorHandler func(s *discordgo.Session, i *discordgo.InteractionCreate, cmd Command, err error)
 
 type rawInteractionHandler func(*discordgo.Session, *discordgo.InteractionCreate)
